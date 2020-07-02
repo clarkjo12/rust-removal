e05122f8379a6dc883826fcd38022b19f27f8ea8
Jon <clarkjo12@gmail.com>
2020-07-01 21:55:23 -0400

    keep-on-pushing

---
+++ logger.js
@@ -0,0 +1,11 @@
+const EvenEmitter = require("events");
+const uuid = require("uuid");
+
+class Logger extends EvenEmitter {
+  log(msg) {
+    //Call event
+    this.emit("message", { id: uuid.v4(), msg });
+  }
+}
+
+module.exports = Logger;
